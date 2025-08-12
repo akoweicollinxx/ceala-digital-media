4520bd7def3a1aceaece54d93f7f8c03240239ea
akoweicollinxx <110723714+akoweicollinxx@users.noreply.github.com>
2025-08-12 11:32:39 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
                    "
           >
             <Link
-              href="/meeting"
+              href="https://whatsform.com/0A2sXo"
               className="py-3 
             px-10
             md:px-16
@@ -113,7 +113,7 @@ export default function Home() {
        duration-200 
        hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
             >
-              Book Call
+              Get Free Website
             </Link>
             <Link
               href={"/showcase"}
