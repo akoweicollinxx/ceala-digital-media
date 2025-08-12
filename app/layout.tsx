d24ceb59d6518920b7057daf32179f6b7de41e5d
akoweicollinxx <110723714+akoweicollinxx@users.noreply.github.com>
2025-08-12 23:14:09 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className={font.className}>
 
         {children}
-  <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/146719206.js"></script>
+  <script type="text/javascript" strategy="afterInteractive" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/146719206.js"></script>
         <Analytics />
 
       </body>
