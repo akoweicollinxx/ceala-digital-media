8119352998f339435b575d1838c4648832dbe300
akoweicollinxx <110723714+akoweicollinxx@users.noreply.github.com>
2025-08-12 10:58:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -95,23 +95,19 @@ export default function Home() {
                    "
           >
             <Link
-              href="/meeting"
-              className="py-3 
-            px-10
-            md:px-16
+              href={"/meeting"}
+              className="
+              bg-white
+   py-3 
+   px-10
+   md:px-16
       md:text-xl
-      hover:bg-[#abcbff] 
-      rounded-[6px]
-      border-4 
-      border-black 
-      dark:border-white 
-       bg-[#121212] 
-       text-white 
-       transition 
-       duration-200 
-       hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
+        border-4
+        border-black
+        rounded-[6px]
+        hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)]"
             >
-              Book a Call
+              book a call
             </Link>
             <Link
               href={"/showcase"}
